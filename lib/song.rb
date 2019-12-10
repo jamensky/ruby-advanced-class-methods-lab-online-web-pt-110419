029c878f5fa4eff9db44ccb4170a4f382cd780ed
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-10 20:51:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,32 +3,38 @@
   class Song
     attr_accessor :name, :artist_name, :song_name
     @@all = []
+    
 
     def self.all
       @@all
     end
+    
 
     def save
       self.class.all << self
     end
+    
 
     def self.create
       new_song = self.new
       new_song.save
       return new_song
     end
+    
 
     def self.new_by_name(name)
       new_song = self.new
       new_song.name = name
       return new_song
     end
+    
 
     def self.create_by_name(name)
        new_song = self.create
        new_song.name = name
        return new_song
     end
+    
 
     def self.find_by_name(name)
       @@all.detect {|song| song.name == name}
